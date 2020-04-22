1d1384497aaad0b504c801b284f6ea4d6580fdce
koushith <koushith97@gmail.com>
2020-04-22 14:32:16 +0530

    final touches

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// dbPassword =
+//   'mongodb+srv://koushith:' +
+//   encodeURIComponent('koushith97!') +
+//   '@Cluster0.mongodb.net/test?retryWrites=true';
+
+// module.exports = {
+//   mongoURI: dbPassword
+// };
